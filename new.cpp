78d7977db59b6e7dd5057f1123c16f2685cfb207
Josue Ttito <josue@inka-labs.co>
2020-07-08 19:21:13 -0500

    fix conflict

--- new.cpp
+++ new.cpp
@@ -84,10 +84,6 @@ int main() {
     auto start_time = std::chrono::system_clock::now();
     int M = 100000;
     int max_key_value = 1000000;
-<<<<<<< HEAD
-=======
-    int queries = 1000000;
->>>>>>> d5eb1e57b4485180bea20b0be6bfe247e506924b
     int range_size = 100000;
     int queries = 100000;
 
